f3d7162f2731b81b5cb0fc3a86fbfb87f6bbe593
Ritesh <dahiteritesh@gmail.com>
2023-07-11 14:39:05 +0530

    Add project files.

---
+++ AashirwadAgenciesWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AashirwadAgenciesWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AashirwadAgenciesWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
